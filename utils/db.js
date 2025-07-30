ea57f9eaa0a60ec5335fe5b57e2db7d622822827
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2025-07-30 12:11:05 +0530

    feat: update database connection to use Sequelize with PostgreSQL; remove MySQL connection code

--- utils/db.js
+++ utils/db.js
@@ -1,28 +1,18 @@
 import "dotenv/config";
 
-// Database connection setup without using ORM
-// import { createConnection } from "mysql2/promise";
-// MySQL Database connection
-// const connection = createConnection({
-//   host: process.env.MYSQL_HOST ,
-//   user: process.env.MYSQL_USER ,
-//   password: process.env.MYSQL_PASSWORD ,
-//   // database: process.env.MYSQL_DATABASE ,
-//   port: process.env.MYSQL_PORT,
-// });
 
 // Database connection setup  using Sequelize ORM
 import {  Sequelize } from "sequelize";
 // Sequelize connection
- const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE,
-  process.env.MYSQL_USER,
-  process.env.MYSQL_PASSWORD,
-  {
-    host: process.env.MYSQL_HOST,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
+
 await sequelize.sync({ alter: true }); // Syncs the model with the database
 
  
